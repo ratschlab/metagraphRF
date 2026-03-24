cac5cb87546a0bdee86cbf3ef3a76d1c2bffaf3e
sayan <sayan.nitd@gmail.com>
2026-03-24 19:30:52 +0100

    logging reads and decisions

--- src/metagraphRF/aligner.py
+++ src/metagraphRF/aligner.py
@@ -1,70 +1,71 @@
 from metagraph.client import GraphClient
+import os
 import sys
 from typing import Optional, Any, Iterable, List, Dict
 from dataclasses import dataclass, fields
 import attrs
 import argparse
 
-@dataclass
-class Params:
-    port: int = 5555
-    min_exact_match: float = 0.5
+# @dataclass
+# class Params:
+#     port: int = 5555
+#     min_exact_match: float = 0.5
 
 
-def get_params_from_kwargs(kwargs):
-    # Filter kwargs to only include fields that are in the Param dataclass
-    param_fields = {f.name for f in fields(Params)}
-    filtered_kwargs = {k: v for k, v in kwargs.items() if k in param_fields}
-    return Params(**filtered_kwargs)
+# def get_params_from_kwargs(kwargs):
+#     # Filter kwargs to only include fields that are in the Param dataclass
+#     param_fields = {f.name for f in fields(Params)}
+#     filtered_kwargs = {k: v for k, v in kwargs.items() if k in param_fields}
+#     return Params(**filtered_kwargs)
 
 
-def get_params_from_args():
-    """
-    Get parameters from sys.argv
-    :return: A Params dataclass instance
-    """
-    parser = argparse.ArgumentParser(description="Readfish-compatible Metagraph API")
+# def get_params_from_args():
+#     """
+#     Get parameters from sys.argv
+#     :return: A Params dataclass instance
+#     """
+#     parser = argparse.ArgumentParser(description="Readfish-compatible Metagraph API")
 
-    # Automatically add arguments based on the Config dataclass fields
-    for field in fields(Params):
-        field_type = field.type
+#     # Automatically add arguments based on the Config dataclass fields
+#     for field in fields(Params):
+#         field_type = field.type
 
-        # For fields that are required, explicitly set 'required=True'
-        if field.default is field.default_factory:  # Check if default is absent
-            is_required = True
-        else:
-            is_required = False
+#         # For fields that are required, explicitly set 'required=True'
+#         if field.default is field.default_factory:  # Check if default is absent
+#             is_required = True
+#         else:
+#             is_required = False
 
-        if field_type == List[str]:
-            # For List fields, handle them as an optional list of values
-            parser.add_argument(
-                f'--{field.name}',
-                type=str,
-                nargs='+',  # Accepts multiple values
-                required=is_required,  # Only set required if field has no default
-                help=f"List of {field.name} values"
-            )
-        elif field_type == bool:
-            # Boolean flags, no value expected
-            parser.add_argument(
-                f'--{field.name}',
-                action='store_true',  # Presence means True
-                required=is_required,  # Only set required if field has no default
-                help=f"Set {field.name} to True"
-            )
-        else:
-            # For other types, handle with default or required flag
-            parser.add_argument(
-                f'--{field.name}',
-                type=field_type,
-                default=getattr(Params, field.name, None),  # Use None if no default
-                required=is_required,  # Only set required if field has no default
-                help=f"Set the {field.name} (default: {getattr(Params, field.name, None)})"
-            )
+#         if field_type == List[str]:
+#             # For List fields, handle them as an optional list of values
+#             parser.add_argument(
+#                 f'--{field.name}',
+#                 type=str,
+#                 nargs='+',  # Accepts multiple values
+#                 required=is_required,  # Only set required if field has no default
+#                 help=f"List of {field.name} values"
+#             )
+#         elif field_type == bool:
+#             # Boolean flags, no value expected
+#             parser.add_argument(
+#                 f'--{field.name}',
+#                 action='store_true',  # Presence means True
+#                 required=is_required,  # Only set required if field has no default
+#                 help=f"Set {field.name} to True"
+#             )
+#         else:
+#             # For other types, handle with default or required flag
+#             parser.add_argument(
+#                 f'--{field.name}',
+#                 type=field_type,
+#                 default=getattr(Params, field.name, None),  # Use None if no default
+#                 required=is_required,  # Only set required if field has no default
+#                 help=f"Set the {field.name} (default: {getattr(Params, field.name, None)})"
+#             )
 
-    parsed_args = parser.parse_args()
-    params = Params(**vars(parsed_args))
-    return params
+#     parsed_args = parser.parse_args()
+#     params = Params(**vars(parsed_args))
+#     return params
 
 
 @dataclass
@@ -107,10 +108,14 @@ class Aligner:
             else:
                 self.logfile = open(debug_log, 'w')
         else:
-            self.logfile = None
+            self.logfile = open(os.devnull, 'w')
         self.kwargs = kwargs
-        self.params = get_params_from_kwargs(kwargs)
-        self.client = GraphClient('localhost', port=self.params.port, api_path='')
+        self.port = self.kwargs.get('port', 5555)
+        self.client_name = self.kwargs.get('name', 'Metagraph_RF_Aligner')
+        self.kwargs.pop('port', None)  # Remove port from kwargs to avoid passing it to GraphClient
+        self.kwargs.pop('name', None)  # Remove name from kwargs to avoid passing it to GraphClient
+        # self.params = get_params_from_kwargs(kwargs)
+        self.client = GraphClient('localhost', api_path='', port=self.port, name=self.client_name)
 
     def validate(self) -> None:
         try:
@@ -144,7 +149,7 @@ class Aligner:
             else:
                 skipped.append(result)
 
-        results = self.client.align(sequences,min_exact_match=self.params.min_exact_match)
+        results = self.client.align(sequences,**self.kwargs)
         aligned_sequences = set(results['seq_description'])
 
         for i in range(len(metadata)):
@@ -152,12 +157,15 @@ class Aligner:
             if str(i) in aligned_sequences:
                 # we send a dummy alignment
                 result.alignment_data = [Alignment('Found', 0, len(result.seq), 1)]
+                print(f"MAP\t{result.read_id}\t{result.seq}", file=self.logfile)
             else:
                 result.alignment_data = []
+                print(f"NOMAP\t{result.read_id}\t{result.seq}", file=self.logfile)
             yield result
 
         for result in skipped:
             result.alignment_data = []
+            print(f"SKIP\t{result.read_id}\tN", file=self.logfile)
             yield result
 
 
@@ -180,7 +188,7 @@ def test_aligner():
 
     aligner = Aligner()
     aligner.validate()
-    df = aligner.client.align(sequences, min_exact_match=aligner.params.min_exact_match)
+    df = aligner.client.align(sequences, min_exact_match=.3)
     print(df)
 
     queries = [Result(i, 'Read' + str(i), sequences[i]) for i in range(len(sequences))]
